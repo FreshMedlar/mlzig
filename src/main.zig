001a82267dfa9630abac4f1127e1747d83430067
FreshMedlar <emaxtiger3000@gmail.com>
2026-03-06 23:47:56 +0100

    var

--- src/main.zig
+++ src/main.zig
@@ -4,17 +4,23 @@ const analysis = @import("analysis.zig");
 const tests = @import("tests.zig");
 
 const NEURON_COUNT: usize = 10000;
+const COMPRESSED_SIZE: usize = 50;
 const MAX_SYNAPSES: usize = 1000000;
+
+const Genotype = struct {
+    compressed_coeffs: [5][COMPRESSED_SIZE]f32,
+};
 
 const Reservoir = struct {
     states: [NEURON_COUNT]f32 = undefined,
+    prev_states: [NEURON_COUNT]f32 = undefined,
     biases: [NEURON_COUNT]f32 = undefined,
     leaks: [NEURON_COUNT]f32 = undefined,
     
     active_indices: [NEURON_COUNT]u32 = undefined,
     active_neuron_count:usize = 0,
     active_mask: [NEURON_COUNT/8]u8 = [_]u8{0} ** (NEURON_COUNT / 8),
-    input_sums: [NEURON_COUNT]f32 = undefined,
+    input_sums: [NEURON_COUNT]f32 = undefined, 
 
     pub fn init() Reservoir {
         return .{
@@ -42,27 +48,39 @@ const SynapsePool = struct {
     weights: [MAX_SYNAPSES]f32 = undefined,
     sources: [MAX_SYNAPSES]u32 = undefined,
     targets: [MAX_SYNAPSES]u32 = undefined,
-    active_count: usize = 0,
+    coeffs: [MAX_SYNAPSES * 5]f32 = undefined,
+    act_syn: usize = 0,
 
-    pub fn addConnection(self: *SynapsePool, src: u32, dst: u32, w: f32) void {
-        if (self.active_count >= MAX_SYNAPSES) return;
+    pub fn addConnection(self: *SynapsePool, src: u32, dst: u32, w: f32, c: [5]f32) void {
+        if (self.act_syn >= MAX_SYNAPSES) return;
 
-        const idx = self.active_count;
+        const idx = self.act_syn;
         self.weights[idx] = w;
         self.sources[idx] = src;
         self.targets[idx] = dst;
-        self.active_count += 1;
+
+        const c_offset = idx * 5;
+        inline for (0..5) |i| {
+            self.coeffs[c_offset + i] = c[i];
+        }
+        self.act_syn += 1;
     }
 
     pub fn pruneConnection(self: *SynapsePool, index: usize) void {
-        if (index >= self.active_count) return;
+        if (index >= self.act_syn) return;
 
-        const last_idx = self.active_count - 1;
+        const last_idx = self.act_syn - 1;
         self.weights[index] = self.weights[last_idx];
         self.sources[index] = self.sources[last_idx];
         self.targets[index] = self.targets[last_idx];
 
-        self.active_count -= 1;
+        const dst_offset = index * 5;
+        const src_offset = last_idx * 5;
+        inline for (0..5) |i| {
+            self.coeffs[dst_offset + i] = self.coeffs[src_offset + i];
+        }
+
+        self.act_syn -= 1;
     }
 
     pub fn rndInit() SynapsePool {
@@ -77,18 +95,28 @@ pub fn updateReservoir(res: *Reservoir) void {
 }
 
 pub fn forward(res: *Reservoir, pool: *SynapsePool) void {
+    // we save res.states for the weights update
+    // note that we may need to save the state before injecting the input
+    @memcpy(res.prev_states, res.states);
+
+    // prepare n input for n active_neurons
     for (res.active_indices[0..res.active_neuron_count]) |idx| {
         res.input_sums[idx] = 0.0;
     }
-
+    
+    // we cycle through all the synapses
     for (
-        pool.weights[0..pool.active_count],
-        pool.sources[0..pool.active_count],
-        pool.targets[0..pool.active_count]
+        pool.weights[0..pool.act_syn],
+        pool.sources[0..pool.act_syn], 
+        pool.targets[0..pool.act_syn]
     ) |w, src, dst| {
         res.input_sums[dst] += w * res.states[src];
     }
 
+    // beware race conditions if modifying this code: the algo must remain order independent,
+    // the loops must be separate, or use a buffer
+    
+    // we compute the actual per-neuron input
     for (res.active_indices[0..res.active_neuron_count]) |idx| {
         const input = res.input_sums[idx];
         const leak = res.leaks[idx];
@@ -99,9 +127,25 @@ pub fn forward(res: *Reservoir, pool: *SynapsePool) void {
     }
 } 
 
-pub fn addConnection(res: *Reservoir, pool: *SynapsePool, src: u32, dst: u32, w: f32) void {
+pub fn applyPlasticity(res: *Reservoir, pool: *SynapsePool) void {
+    for (0..pool.act_syn) |i| {
+        const src = pool.sources[i];
+        const dst = pool.targets[i];
+        const pre = res.prev_states[src];
+        const post = res.states[dst];
+
+        const c = pool.coeffs[i * 5 .. i * 5 + 5];
+        const delta = c[0] * (c[1]*pre*post + c[2]*pre + c[3]*post + c[4]);
+
+        pool.weights[i] += delta;
+
+        // may need to clamp weights for divergence
+    }
+}
+
+pub fn addConnection(res: *Reservoir, pool: *SynapsePool, src: u32, dst: u32, w: f32, c: [5]f32) void {
     // 1. Add the synapse to the pool
-    pool.addConnection(src, dst, w);
+    pool.addConnection(src, dst, w, c);
 
     // 2. Register these neurons as active
     // This adds them to the active_indices list if they aren't there already
@@ -110,6 +154,8 @@ pub fn addConnection(res: *Reservoir, pool: *SynapsePool, src: u32, dst: u32, w:
 }
 
 pub fn initialize(res: *Reservoir, pool: *SynapsePool, active_count: u16) void {
+    const default_coeffs = [_]f32{0.0} ** 5;
+
     // we mark the N as active 
     for (0..active_count) |i| {
         res.markActive(@intCast(i));
@@ -117,9 +163,49 @@ pub fn initialize(res: *Reservoir, pool: *SynapsePool, active_count: u16) void {
     // create the synapses
     for (0..active_count) |n| {
         for (0..active_count) |m| {
-            addConnection(res, pool, @intCast(n), @intCast(m), 0.1);
+            addConnection(res, pool, @intCast(n), @intCast(m), 0.1, default_coeffs);
         }
     }
+}
+
+// make this function generic
+pub fn expand_genome(geno: [50]f32, pheno: *[1000]f32) void {
+    for (0..1000) |i| {
+        var val: f32 = 0.0;
+        const x = @as(f32, @floatFromInt(i)) / 1000.0;
+
+        for (geno, 0..) |amp, freq| {
+            const f = @as(f32, @floatFromInt(freq + 1));
+            val += amp * std.math.cos(f * std.math.pi * x);
+        }
+        pheno[i] = val;
+    }
+}
+
+pub fn fitness(
+    original_pool: *const SynapsePool, 
+    perturbation: [5000]f32,
+    input_data: []f32
+) f32 {
+    var worker_pool = original_pool.*;
+
+    for (perturbation, 0..) |p, i| {
+        worker_pool.coeffs[i] += p;
+    }
+
+    var worker_res = Reservoir.init();
+
+    var total_error: f32 = 0.0;
+    for(input_data) |val| {
+        worker_res.states[0] = val;
+        forward(&worker_res, &worker_pool);
+        applyPlasticity(&worker_res, &worker_pool);
+
+        // TODO readout
+        total_error += 0.01;
+    }
+
+    return total_error;
 }
 
 // --------------------------------------------------------------------------
@@ -149,11 +235,13 @@ pub fn main() !void {
 
     initialize(&reservoir, &synapses, 10);
     
-    for (0..99999) |i| {
+    for (0..99) |i| {
         // input
         reservoir.states[0] = mg.next();
 
         forward(&reservoir, &synapses);
+        applyPlasticity(&reservoir, &synapses);
+
         if (i % 100 == 0) {
             for (0..5) |n| {std.debug.print("{d:.4}", .{reservoir.states[n]});}
             std.debug.print("\n", .{});
